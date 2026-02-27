3cb90ccd9ef8aceff00f20cad75cb33ba8830a90
taiseiue <taiseiue@wsnet.jp>
2026-02-27 22:09:38 +0900

    nixでも使えるようにしておく

--- packages/primitive-wasm/build.sh
+++ packages/primitive-wasm/build.sh
@@ -2,5 +2,8 @@
 cd "$(dirname "$0")"
 
 GOOS=js GOARCH=wasm go build -o primitive.wasm .
-cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" ../frontend/public/
+
+GOROOT=$(go env GOROOT)
+WASM_EXEC_JS_PATH=$(find $GOROOT -name "wasm_exec.js")
+cp "$WASM_EXEC_JS_PATH" ../frontend/public/
 cp primitive.wasm ../frontend/public/
